28b1db6373f8b7be94e2c2c5213dfb99f9871002
br-sagar <64830119+br-sagar@users.noreply.github.com>
2020-05-12 12:12:25 +0530

    Table
    Changes in table design

--- react-demo/src/Components/splitPane.jsx
+++ react-demo/src/Components/splitPane.jsx
@@ -1,86 +1,114 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 function LeftPane(props) {
-    return (
-        <div style={{ borderRadius: "20px 0px 0px 20px", backgroundColor: "blue" }}>
-            {props.children}
-        </div>
-    );
+  return (
+    <div style={{ borderRadius: "20px 0px 0px 20px", backgroundColor: "blue" }}>
+      {props.children}
+    </div>
+  );
 }
 
 function PostComment() {
-    return (
-        <div>
-            <textarea></textarea>
-        </div>
-    );
+  return (
+    <div>
+      <textarea />
+    </div>
+  );
 }
 function SocialActivity(props) {
-    return (
-        <ul className="socialLinks">
-            <li>
-                <span className="badge badge-pill badge-light mt-2" style={{ fontSize: 13 }}>
-                    {props.likes} {props.likes == 0 ? "Like" : "Likes"}
-                </span>
-            </li>
-            <li>
-                <a href="javascript:void(0);" onClick={props.handleLikes}>
-                    <img className="mr-3 " src="https://img.icons8.com/cute-clipart/64/000000/facebook-like.png" style={{ height: 32, width: 32 }} />
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img className="mr-3 " src="https://img.icons8.com/color/48/000000/comments.png" style={{ height: 32, width: 32 }} />
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="https://img.icons8.com/flat_round/64/000000/share--v1.png" style={{ height: 32, width: 32 }} />
-                </a>
-            </li>
-        </ul>
-    );
+  return (
+    <ul className="socialLinks">
+      <li>
+        <span
+          className="badge badge-pill badge-light mt-2"
+          style={{ fontSize: 13 }}
+        >
+          {props.likes} {props.likes === 0 ? "Like" : "Likes"}
+        </span>
+      </li>
+      <li>
+        <button
+          onClick={props.handleLikes}
+          className="btn btn-link btn-sm socialButtonn"
+        >
+          <img
+            className="mr-3 "
+            src="https://img.icons8.com/cute-clipart/64/000000/facebook-like.png"
+            style={{ height: 32, width: 32 }}
+            alt="likes"
+          />
+        </button>
+      </li>
+      <li>
+        <button className="btn btn-link btn-sm socialButtonn">
+          <img
+            className="mr-3 "
+            src="https://img.icons8.com/color/48/000000/comments.png"
+            style={{ height: 32, width: 32 }}
+            alt="comment"
+          />
+        </button>
+      </li>
+      <li>
+        <button className="btn btn-link btn-sm socialButtonn">
+          <img
+            src="https://img.icons8.com/flat_round/64/000000/share--v1.png"
+            style={{ height: 32, width: 32 }}
+            alt="share"
+          />
+        </button>
+      </li>
+    </ul>
+  );
 }
 
 function RightPane(props) {
-    return (
-        <div className="col-lg-7" style={{ borderRadius: "0px 20px 20px 0px", backgroundColor: "#e3f2fd", fontSize: 13 }}>
-            {props.children}
-        </div>
-    );
+  return (
+    <div
+      className="col-lg-7"
+      style={{
+        borderRadius: "0px 20px 20px 0px",
+        backgroundColor: "#e3f2fd",
+        fontSize: 13
+      }}
+    >
+      {props.children}
+    </div>
+  );
 }
 
 class SplitPane extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            likes: 0
-        }
-    }
-    likeIncreament = () => {
-        this.setState({
-            likes: this.state.likes + 1
-        })
-    }
-    render() {
-        return (
-            <div className="row justify-content-center m-3 clearfix">
-                <LeftPane>
-                    <div>
-                        <img src={this.props.image} className="thumbnail" />
-                    </div>
-                </LeftPane>
-                <RightPane >
-                    <div>
-                        <section>
-                            {this.props.children}
-                        </section>
-                        <SocialActivity handleLikes={this.likeIncreament} likes={this.state.likes} />
-                    </div>
-                </RightPane>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0
+    };
+  }
+  likeIncreament = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  };
+  render() {
+    return (
+      <div className="row justify-content-center m-3 clearfix">
+        <LeftPane>
+          <div>
+            <img src={this.props.image} className="thumbnail" />
+          </div>
+        </LeftPane>
+        <RightPane>
+          <div>
+            <section>{this.props.children}</section>
+            <SocialActivity
+              handleLikes={this.likeIncreament}
+              likes={this.state.likes}
+            />
+          </div>
+        </RightPane>
+      </div>
+    );
+  }
 }
 
 export default SplitPane;
--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,78 +1,95 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './Components/app'
-
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./Components/app";
 
 const name = {
-    'firstName': 'Sagar',
-    'lastName': 'Vishwakarma'
+  firstName: "Sagar",
+  lastName: "Vishwakarma"
 };
 
 function ModifyName(params) {
-    return params.firstName + " " + params.lastName;
+  return params.firstName + " " + params.lastName;
 }
 //created app component which is named as Intro that will be render in UI.
 function Intro() {
-    const element = (
-        <div id="content">
-            <h3>Time is {new Date().toLocaleString()}</h3>
-            <h3 style={{ color: "tomato", fontFamily: 'Times New Roman', fontSize: 30, textAlign: "center" }} className="app">This app is created by {ModifyName(name)}</h3>
-            <Description name="Facebook" year="2011" />
-        </div>);
-    return element;
+  const element = (
+    <div id="content">
+      <h3>Time is {new Date().toLocaleString()}</h3>
+      <h3
+        style={{
+          color: "tomato",
+          fontFamily: "Times New Roman",
+          fontSize: 30,
+          textAlign: "center"
+        }}
+        className="app"
+      >
+        This app is created by {ModifyName(name)}
+      </h3>
+      <Description name="Facebook" year="2011" />
+    </div>
+  );
+  return element;
 }
 function Description(props) {
-    const element = (
-        <div style={{ marginTop: '20', width: '60%', height: '100%' }} >
-            <p>It is a react app which implements the concept of component and property which is a feature of
-                React JS. It is a <strong>JavaScript Library </strong> which is developed by {props.name} in <strong>{props.year}</strong>
-            </p>
-        </div>
-    );
-    return element;
+  const element = (
+    <div style={{ marginTop: "20", width: "60%", height: "100%" }}>
+      <p>
+        It is a react app which implements the concept of component and property
+        which is a feature of React JS. It is a{" "}
+        <strong>JavaScript Library </strong> which is developed by {props.name}{" "}
+        in <strong>{props.year}</strong>
+      </p>
+    </div>
+  );
+  return element;
 }
 
 function Poster(props) {
-    useState()
-    return (
-        <img src={props.posterUrl} className="Poster" />
-    );
+  useState();
+  return <img src={props.posterUrl} className="Poster" alt={props.altText} />;
 }
 
 function MovieDetails(props) {
-    return (
-        <div >
-            <Poster posterUrl={props.movieDetail.PosterUrl} />
-            <div className="details">
-                <label>Name:</label>{props.movieDetail.Name}<br />
-                <label>Release Date:</label>{props.movieDetail.ReleaseDate}<br />
-                <label>Star Cast:</label>{props.movieDetail.Cast}<br />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Poster posterUrl={props.movieDetail.PosterUrl} altText="kick" />
+      <div className="details">
+        <label>Name:</label>
+        {props.movieDetail.Name}
+        <br />
+        <label>Release Date:</label>
+        {props.movieDetail.ReleaseDate}
+        <br />
+        <label>Star Cast:</label>
+        {props.movieDetail.Cast}
+        <br />
+      </div>
+    </div>
+  );
 }
 function Movie(props) {
-    return (
-        <div style={{ width: '200px' }} className="movie shadow">
-            <React.Fragment>
-                <MovieDetails movieDetail={props.movie}></MovieDetails>
-            </React.Fragment>
-        </div>
-    );
+  return (
+    <div style={{ width: "200px" }} className="movie shadow">
+      <React.Fragment>
+        <MovieDetails movieDetail={props.movie} />
+      </React.Fragment>
+    </div>
+  );
 }
 const movie = {
-    PosterUrl: "https://upload.wikimedia.org/wikipedia/en/b/b6/Kick_%282014_film%29_Official_release_poster.jpg",
-    Cast: "Salman Khan, Jacqualine Fernandiz",
-    ReleaseDate: "21/01/2020",
-    Name: "Kick"
-}
-//Here we are consuming the Intro component which we created earlier 
+  PosterUrl:
+    "https://upload.wikimedia.org/wikipedia/en/b/b6/Kick_%282014_film%29_Official_release_poster.jpg",
+  Cast: "Salman Khan, Jacqualine Fernandiz",
+  ReleaseDate: "21/01/2020",
+  Name: "Kick"
+};
+//Here we are consuming the Intro component which we created earlier
 //ReactDOM.render(<Movie movie={movie} />, document.getElementById("root"));
 
 //An example of passing property to the function component.
 //ReactDOM.render(<Description name="Facebook" year="2011" />, document.getElementById("content"));
-
 
 ReactDOM.render(<App />, document.getElementById("content"));
